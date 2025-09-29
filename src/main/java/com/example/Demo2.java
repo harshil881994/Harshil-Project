47e51e28cdfe015f9faf30ec7677493a20f15a76
harshil881994 <50701881+harshil881994@users.noreply.github.com>
2025-09-29 21:48:38 +0530

    Update Demo2.java

--- src/main/java/com/example/Demo2.java
+++ src/main/java/com/example/Demo2.java
@@ -3,6 +3,7 @@ package com.example;
 public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		System.out.println("Harshil");
 		System.out.println("Kenil");
 	}
 
